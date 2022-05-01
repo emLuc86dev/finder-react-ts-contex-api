d1188e32285972e07564e4f6b5b28ee49a7c1415
emLuc86dev <86974125+emLuc86dev@users.noreply.github.com>
2022-05-01 22:56:43 +0200

    fixing about component

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -11,19 +11,14 @@ function About() {
           </a>{' '}
           Udemy course by
           <strong>
-            <a href='https://traversymedia.com'> Brad Traversy</a>
+            <a href='https://traversymedia.com'>Emilio Salvia</a>
           </strong>
           .
         </p>
         <p className='text-lg text-gray-400'>
           Version <span className='text-white'>1.0.0</span>
         </p>
-        <p className='text-lg text-gray-400'>
-          Layout By:
-          <a className='text-white' href='https://twitter.com/hassibmoddasser'>
-            Hassib Moddasser
-          </a>
-        </p>
+        
       </>
     )
   }
